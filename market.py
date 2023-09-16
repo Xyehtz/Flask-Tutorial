5ce0084e5ea452f6c6d646b1536daf249342968c
Xyehtz <alejitogarbo@gmail.com>
2023-09-15 20:24:13 -0400

    Added HTML, and a Navbar

--- market.py
+++ market.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/', )
-def hello_world():
-  return '<h1>Hello World!\n POPIPOPIPOPOPIPO</h1>'
+@app.route('/')
+@app.route('/home')
+def home_page():
+  return render_template('home.html')
 
+@app.route('/market')
+def market_page():
+  items = [
+    {'id': 1, 'name': 'Phone', 'barcode': '89321299897', 'price': 500},
+    {'id': 2, 'name': 'Laptop', 'barcode': '82648827492', 'price': 900},
+    {'id': 3, 'name': 'Keyboard', 'barcodee': '74820847520', 'price': 150}
+  ]
+  return render_template('market.html', items=items)
+
+# Possible easter egg
 # @app.route('/about/<username>')
 # def about_page(username):
 #   # return '<h1>Smurf Cat</h1><img src=https://i.kym-cdn.com/photos/images/newsfeed/002/652/460/d70.jpg height=440px width=440px alt=Smurf Cat/>\n<a href=http://127.0.0.1:5000/>Hello World</a>'
